077bb2fea0370b7861e29abc88011302908d61d4
nehemiahk <nehemiahkiv@gmail.com>
2020-06-11 15:01:14 +0300

    Updates

---
+++ precache-manifest.07542abc01b25bf969894f4c3e40886c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b795036c864c5fe1f8c7c8979e3ee",
+    "url": "/pomo-clock/index.html"
+  },
+  {
+    "revision": "11421d128e7e8450dd2c",
+    "url": "/pomo-clock/static/css/main.322c605a.chunk.css"
+  },
+  {
+    "revision": "6b5d46f68af666feb871",
+    "url": "/pomo-clock/static/js/2.a1733d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomo-clock/static/js/2.a1733d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11421d128e7e8450dd2c",
+    "url": "/pomo-clock/static/js/main.7331fd27.chunk.js"
+  },
+  {
+    "revision": "1857862e78960fc79a96",
+    "url": "/pomo-clock/static/js/runtime-main.106738da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomo-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
